8caead4d48cd21cd1aa0c97d4ad855b881e0d88b
yvygyyth <1589937631@qq.com>
2026-01-15 14:38:15 +0800

    feat: 修改配置文件

--- electron/main/hooks/useUpdater.ts
+++ electron/main/hooks/useUpdater.ts
@@ -26,6 +26,21 @@ autoUpdater.on('download-progress', (progress) => {
     console.log(progress)
 })
 
+// 更新下载完成
+autoUpdater.on('update-downloaded', (info) => {
+    dialog
+        .showMessageBox({
+            title: '更新下载完成',
+            message: '更新下载完成，是否重启应用？',
+            buttons: ['重启', '取消']
+        })
+        .then((results) => {
+            if (results.response === 0) {
+                autoUpdater.quitAndInstall()
+            }
+        })
+})
+
 export const useUpdater = () => {
     const checkForUpdates = async () => {
         try {
